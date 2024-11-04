c5a514f9fd0e2b464e81a84aa9297d9d90d8a7fe
micha <michael-kick@gmx.de>
2024-11-04 15:45:30 +0100

    positions added

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ def create_chess_board():
     return board
 
 def set_up_start_position(board):
-    black_pawn_pos = ["a2" , "b2", "c2","d2","e2","f2" ,"g2","h2"]
-    black_rook_pos = []
+    # Weiße Figuren (unten auf dem Brett)
+    white_pawn_pos = ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"]
+    white_rook_pos = ["a1", "h1"]
+    white_knight_pos = ["b1", "g1"]
+    white_bishop_pos = ["c1", "f1"]
+    white_queen_pos = ["d1"]
+    white_king_pos = ["e1"]
 
-
-    white_pawn_pos = ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]
+    # Schwarze Figuren (oben auf dem Brett)
+    black_pawn_pos = ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]
+    black_rook_pos = ["a8", "h8"]
+    black_knight_pos = ["b8", "g8"]
+    black_bishop_pos = ["c8", "f8"]
+    black_queen_pos = ["d8"]
+    black_king_pos = ["e8"]
 
     for field in board:
         # black pawns
