f56e156f817d5aaf96edc167b6177b8fa5f0cd29
micha <michael-kick@gmx.de>
2024-11-04 15:42:05 +0100

    positions added

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+def create_chess_board():
+    helper: str = "abcdefgh"
+    board = []
+    for row in range(0, 8):
+        for col in range(8,0,-1):
+            board.append((
+                helper[row] + str(col), "", ""
+            ))
+    return board
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def set_up_start_position(board):
+    black_pawn_pos = ["a2" , "b2", "c2","d2","e2","f2" ,"g2","h2"]
+    black_rook_pos = []
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    white_pawn_pos = ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]
+
+    for field in board:
+        # black pawns
+        if field[0] == 7:
+            field[1] = "pawn"
+            field[2] = "black"
+
+        # white pawns
+        if field[0] == 2:
+            field[1] = "pawn"
+            field[2] = "white"
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    chessboard = create_chess_board()
+    set_up_start_position(chessboard)
