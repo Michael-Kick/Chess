6070fab1a9f09e2ee57e3ceb66140a4954b93058
micha <michael-kick@gmx.de>
2024-11-05 10:39:38 +0100

    chess board visualized

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
+from array import ArrayType
+from operator import index
+
+
+def print_chess_board(board):
+    row: str = ""
+    counter: int = 1
+    for position,piece in board.items():
+        if counter % 8 == 0:
+            print(row)
+            row = ""
+        else:
+            row = row + "|" + str(position) + str(piece)
+        counter = counter + 1
+
+
 def create_chess_board():
     helper: str = "abcdefgh"
-    board = []
-    for row in range(0, 8):
-        for col in range(8,0,-1):
-            board.append((
-                helper[row] + str(col), "", ""
-            ))
+    board = {}
+    for col in range(8, 0, -1):
+        for row in range(0, 8):
+            board[helper[row] + str(col)] = ("", "")
     return board
 
+
 def set_up_start_position(board):
-    # Weiße Figuren (unten auf dem Brett)
-    white_pawn_pos = ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"]
-    white_rook_pos = ["a1", "h1"]
-    white_knight_pos = ["b1", "g1"]
-    white_bishop_pos = ["c1", "f1"]
-    white_queen_pos = ["d1"]
-    white_king_pos = ["e1"]
+    chess_pieces = [
+        (["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"], "pawn", "white"),
+        (["a1", "h1"], "rook", "white"),
+        (["b1", "g1"], "knight", "white"),
+        (["c1", "f1"], "bishop", "white"),
+        (["d1"], "queen", "white"),
+        (["e1"], "king", "white"),
 
-    # Schwarze Figuren (oben auf dem Brett)
-    black_pawn_pos = ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]
-    black_rook_pos = ["a8", "h8"]
-    black_knight_pos = ["b8", "g8"]
-    black_bishop_pos = ["c8", "f8"]
-    black_queen_pos = ["d8"]
-    black_king_pos = ["e8"]
+        (["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"], "pawn", "black"),
+        (["a8", "h8"], "rook", "black"),
+        (["b8", "g8"], "knight", "black"),
+        (["c8", "f8"], "bishop", "black"),
+        (["d8"], "queen", "black"),
+        (["e8"], "king", "black")
+    ]
 
-    for field in board:
-        # black pawns
-        if field[0] == 7:
-            field[1] = "pawn"
-            field[2] = "black"
+    for positions, piece, colour in chess_pieces:
+        for position in positions:
+            board[position] = (piece, colour)
 
-        # white pawns
-        if field[0] == 2:
-            field[1] = "pawn"
-            field[2] = "white"
+    print_chess_board(board)
 
 
 if __name__ == '__main__':
